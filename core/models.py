b07fbc70ad443a46608a0e73b8e89760d417cb96
Charlie-cu <carlosjorgelj98@gmail.com>
2024-02-19 17:45:00 -0500

    19022024

--- core/models.py
+++ core/models.py
@@ -1,18 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class User(models.Model):
-    pass
-class Rol(models.Model):
+class Admin(User):
     roles={
         'admin':'administrador',
         'estudiante':'estudiante',
         'profesor':'profesor',
     }
-    nombre_rol=models.CharField(max_lenght=20,choices=roles)
+    rol=models.CharField(max_length=20,choices=roles)
+    
+    
+class Docente(User):
+    roles={
+        'admin':'administrador',
+        'estudiante':'estudiante',
+        'profesor':'profesor',
+    }
+    rol=models.CharField(max_length=20,choices=roles)
+    dni = models.IntegerField()
+    
+    
+class Estudiante(User):
+    roles={
+        'admin':'administrador',
+        'estudiante':'estudiante',
+        'profesor':'profesor',
+    }
+    rol=models.CharField(max_length=20,choices=roles)
+    calificacion = models.IntegerField()
+    
     
 class Subject(models.Model):
     #asignatura
-    pass
-
-class Curso(models.Model):
-    pass
+    estudiante=models.ManyToManyField(Estudiante)
+    docente=models.OneToOneField(Docente,on_delete=models.CASCADE)
+    sesion={"mañana":"mañana","tarde":"tarde"}
+    nombre_completo = models.CharField(max_length=19)
+    sesion=models.CharField(max_length=19,choices=sesion)
+    
+class Examen(models.Model):
+    nom_asig=models.CharField(max_length=20)
+    fecha=models.DateTimeField(auto_now_add=True)
+    lugar=models.CharField(max_length=20)
